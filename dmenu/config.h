bf849f194759eeb3436c23635c5262407abb6521
Apostolos Piperis <apostolostpiperis@gmail.com>
2022-11-01 00:08:38 +0200

    Fix some typo when editing, probably.

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,19 +2,26 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 700;                 /* minimum width when centered */
+static int passwd_char = '*';				/* -p option; char used as char replacment */
+static double dim_opacity = 0;	     		/* -d option; if 0, no dimming takes place */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#9e96b6" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeDim] = { "#000000", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 18;
+static unsigned int lineheight = 24;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
